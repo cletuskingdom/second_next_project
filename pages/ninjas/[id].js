0fa3097e7d240b6ace7a922ef12bf5f3b7a50bcb
Cletus Kingdom <cletuskingdom@gmail.com>
2022-05-21 13:40:06 +0100

    updated the ninja's dynamic page

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -2,22 +2,22 @@ export const getStaticPaths = async () => {
 	const res = await fetch("https://jsonplaceholder.typicode.com/users");
 	const data = await res.json();
 
-	const path = data.map((ninja) => {
+	const paths = data.map((ninja) => {
 		return {
 			params: { id: ninja.id.toString() },
 		};
 	});
 
 	return {
-		path,
-		fallback: false, // If the id don't exist
+		paths,
+		fallback: false,
 	};
 };
 
 export const getStaticProps = async (context) => {
 	const id = context.params.id;
-	const res = fetch("https://jsonplaceholder.typicode.com/users/" + id);
-	const data = await (await res).json();
+	const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+	const data = await res.json();
 
 	return {
 		props: { ninja: data },
